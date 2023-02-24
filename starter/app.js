d9016782b6110da82b34aff0ae6c2e519b431611
Giorgi <giorgizanqaidzeOfficial@gmail.com>
2023-02-24 10:04:52 +0400

    first commit

---
+++ starter/app.js
@@ -0,0 +1,43 @@
+require("dotenv").config()
+// async errors
+
+const express = require('express')
+
+const app = express()
+
+const connectDB = require('./db/connect')
+
+const notFoundMiddleware = require('./middleware/not-found')
+const errorMiddleware = require('./middleware/error-handler')
+
+
+
+//middleware
+app.use(express.json())
+
+//rootes
+
+app.get('/', (req, res) => {
+    res.send('<h1>store API</h1><a href="/api/v1/products">products route</a>')
+})
+
+// products route
+
+app.use(notFoundMiddleware)
+app.use(errorMiddleware)
+
+const port = process.env.PORT || 3000
+
+
+const start = async () => {
+    try {
+        //connectDB
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log('server is listening'))
+
+    } catch(error) {
+        console.log(error)
+    } 
+}
+
+start()
