747e1c1f76f9e9e9252a57ef44b9c34a9dbb4b1c
Giorgi <giorgizanqaidzeOfficial@gmail.com>
2023-02-25 16:56:31 +0400

    store products in database

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,11 +1,13 @@
-
+const Products = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    res.status(200).json({msg: 'products testing routes static' })
+    const products = await Products.find({})
+    res.status(200).json({ products })
 }
 
 const getAllProducts = async (req, res) => {
-    res.status(200).json({ msg: 'products route' })
+
+    res.status(200).json({ msg: "hehehe" })
 }
 
 module.exports = {
