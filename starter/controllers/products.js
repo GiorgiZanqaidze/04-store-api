305c9ea7ab978517fb4b66af37ff9da854eb6318
Giorgi <giorgizanqaidzeOfficial@gmail.com>
2023-03-02 10:25:51 +0400

    create query object

--- starter/controllers/products.js
+++ starter/controllers/products.js
@@ -1,13 +1,41 @@
 const Products = require('../models/product')
 
 const getAllProductsStatic = async (req, res) => {
-    const products = await Products.find({})
-    res.status(200).json({ products })
+
+    const products = await Products.find({}).select('name price')
+    res.status(200).json({ nbHints: products.length, products })
 }
 
 const getAllProducts = async (req, res) => {
+    const {featured, company, name, sort} = req.query
 
-    res.status(200).json({ msg: "hehehe" })
+    const queryObject = {}
+
+    if (featured) {
+        queryObject.featured = featured === "true" ? true : false
+    }
+
+    if (company) {
+        queryObject.company = company 
+    }
+
+    if (name) {
+        queryObject.name = { $regex: name, $options: 'i'}
+    }
+
+    let result = Products.find(queryObject)
+    
+    if (sort) {
+        const sortList = sort.split(',').join(' ');
+        result = result.sort(sortList);
+    } else {
+        result = result.sort('createdAt');
+    }
+    console.log(queryObject)
+    
+    const products = await result
+
+    res.status(200).json({ nbHints: products.length, products })
 }
 
 module.exports = {
